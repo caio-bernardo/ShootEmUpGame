cf2ae00d1d21d347d14557d3117c95167d7f3434
Caio Bernardo <caio.2004vb@gmail.com>
2025-06-11 14:30:41 -0300

    feat: add Renderable interface

--- app/src/main/java/org/shootemup/components/Vector2D.java
+++ app/src/main/java/org/shootemup/components/Vector2D.java
@@ -3,4 +3,17 @@ package org.shootemup.components;
 import java.awt.geom.Point2D;
 
 // Representa um vetor em um espaço vetorial R^2
-public class Vector2D extends Point2D.Double {}
+public class Vector2D extends Point2D.Double {
+
+    public Vector2D(double x, double y) {
+        super(x, y);
+    }
+
+    public static Vector2D ofScalar(double t) {
+        return new Vector2D(t, t);
+    }
+
+    public Vector2D addScalar(double s) {
+        return new Vector2D(x + s, y + s);
+    }
+}
